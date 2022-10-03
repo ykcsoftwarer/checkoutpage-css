0268df8f5938a4d36c40785e6c6bb09b3f1dcd57
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-10-03 07:57:00 +0300

    maded capturing

--- app.js
+++ app.js
@@ -5,17 +5,35 @@ const kargoUcreti = 15;
 const kargoBedavaUcreti =300;
 
 window.addEventListener("load", () => {
-    localStorage.setItem("kdvUcreti", "kdv")
-    localStorage.setItem("kargoUcreti", "kargo")
-    localStorage.setItem("kargoBedavaUcreti", "bedava")
+    localStorage.setItem("kdvUcreti", "kdv");
+    localStorage.setItem("kargoUcreti", "kargo");
+    localStorage.setItem("kargoBedavaUcreti", "bedava");
 
-    sessionStorage.setItem("kdvUcreti", "kdv")
-    sessionStorage.setItem("kargoUcreti", "kargo")
-    sessionStorage.setItem("kargoBedavaUcreti", "bedava")
-    
+    sessionStorage.setItem("kdvUcreti", "kdv");
+    sessionStorage.setItem("kargoUcreti", "kargo");
+    sessionStorage.setItem("kargoBedavaUcreti", "bedava");
+
 
 });
 
+//
 
+const productsDiv = document.querySelector(".products");
+productsDiv.addEventListener("click", (event) => {
+    // console.log(event);
+    if(event.target.className == "fa-solid fa-minus") {
+        //console.log("eksi butonu çalışıyor");
+    }
+    else if(event.target.className == "fa-solid fa-plus") {
+       console.log("artı butonu çalışıyor"); 
+       //console.log("event.target.previousElementSibling.innerText");
+       event.target.previousElementSibling.innerText++;
+    }
+    else if(event.target.classList.contains("remove-product")) {
+        console.log("remove btn basıldı");
+    }
+        
+
+})
 
 
