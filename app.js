c284672e5aa758d0819ef360369b80e05862f73e
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-10-03 07:25:40 +0300

    checkout-page devam ediyor

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("app.js baglandı");
+
+const kdvUcreti = 0.18;
+const kargoUcreti = 15;
+const kargoBedavaUcreti =300;
+
+window.addEventListener("load", () => {
+    localStorage.setItem("kdvUcreti", "kdv")
+    localStorage.setItem("kargoUcreti", "kargo")
+    localStorage.setItem("kargoBedavaUcreti", "bedava")
+
+    sessionStorage.setItem("kdvUcreti", "kdv")
+    sessionStorage.setItem("kargoUcreti", "kargo")
+    sessionStorage.setItem("kargoBedavaUcreti", "bedava")
+    
+
+});
+
+
+
+
